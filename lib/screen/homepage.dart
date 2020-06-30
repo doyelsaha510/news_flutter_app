f97290f6302a740cb52d9dc8aa5c8cc427b4928c
Doyel Saha <doyelsaha99@gmail.com>
2020-06-30 23:03:17 +0530

    Initial commit

---
+++ lib/screen/homepage.dart
@@ -0,0 +1,70 @@
+
+import 'package:flutter/material.dart';
+import 'package:news_flutter_app/helper/widgets.dart';
+
+import '../helper/news.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  bool _loading;
+  var newslist;
+
+
+
+  void getNews() async {
+    News news = News();
+    await news.getNews();
+    newslist = news.news;
+    setState(() {
+      _loading = false;
+    });
+  }
+
+  @override
+  void initState() {
+    _loading = true;
+    // TODO: implement initState
+    super.initState();
+
+
+    getNews();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: MyAppBar(),
+      body: SafeArea(
+        child: _loading
+            ? Center(
+          child: CircularProgressIndicator(),
+        )
+            : SingleChildScrollView(
+          child: Container(
+            margin: EdgeInsets.only(top: 16),
+            child: ListView.builder(
+                itemCount: newslist.length,
+                shrinkWrap: true,
+                physics: ClampingScrollPhysics(),
+                itemBuilder: (context, index) {
+                  return NewsTile(
+                    imgUrl: newslist[index].urlToImage ?? "",
+                    title: newslist[index].title ?? "",
+                    desc: newslist[index].description ?? "",
+                    content: newslist[index].content ?? "",
+                    posturl: newslist[index].articleUrl ?? "",
+                  );
+                }),
+          ),
+
+        ),
+      ),
+    );
+  }
+}
+
